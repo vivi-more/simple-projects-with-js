7967454dca5f7901a4e8e510065d87d4eb1df681
Vivi-Mores <vmoreira.profissional@gmail.com>
2023-10-10 13:43:17 -0300

    Note Project

---
+++ notes_app/script.js
@@ -0,0 +1,55 @@
+const noteList = document.querySelector(".list");
+const addNoteLi = document.querySelector(".add-note");
+const addNoteButton = document.getElementById("btn-add-note");
+
+let listLocalStorage = [];
+let countList = -1;
+
+fullNotes();
+addNoteButton.addEventListener("click", createNote.bind(null, ""));
+
+function fullNotes() {
+    listLocalStorage = JSON.parse(localStorage.getItem("listLocalStorage"));
+
+    listLocalStorage.forEach(text => {
+        createNote(text);
+    });
+
+}
+
+function createNote(text) {
+    countList++;
+
+    const newLi = document.createElement("li");
+    const newTextArea = document.createElement("textarea");
+
+    newTextArea.setAttribute("name", "comment");
+    newTextArea.addEventListener("change", changeTextAreaText.bind(newTextArea, countList));
+    newTextArea.value = text;
+
+    newLi.addEventListener("dblclick", deleteNote.bind(newLi, countList));
+    newLi.appendChild(newTextArea);
+
+    noteList.insertBefore(newLi, addNoteLi);
+}
+
+
+function deleteNote(position) {
+    countList--;
+
+    const li = this;
+    noteList.removeChild(li);
+
+    listLocalStorage.splice(position, 1);
+    changeLocalStorage();
+}
+
+function changeTextAreaText(position) {
+    const textareaValue = this.value;
+    listLocalStorage[position] = textareaValue;
+    changeLocalStorage();
+}
+
+function changeLocalStorage() {
+    localStorage.setItem("listLocalStorage", JSON.stringify(listLocalStorage));
+}
